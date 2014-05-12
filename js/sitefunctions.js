cfe4b854b3edf01bce1147c25f7fb918c225273e
Squiddite <junk@squiddite.net>
2014-05-12 13:39:30 -0400

    more cloning improvements

--- js/sitefunctions.js
+++ js/sitefunctions.js
@@ -1,3 +1,4 @@
+// calculation button handler
 $( function() {
    $( "#form1 :submit" ).click( function( event ) {
       event.preventDefault();
@@ -9,30 +10,32 @@ $( function() {
    });
 });
 
+// set up template row for cloning; clone button handler
 $( function() {
+   $( ".prototype-row" ).toggle();
    $.getJSON( "ajax.php?get=tankList", function( result ) {
       $.each( result, function( tank ) {
          $( ".prototype-row #tank-list" ).append( $( "<option />" ).val( result[tank].id ).text( result[tank].name ));
       });
+
+      cloneRow();
+      $( "#add-row" ).click( function() { cloneRow(); });
    });
-   $( ".prototype-row" ).toggle();
 });
 
+// row cloner
+function cloneRow() {
+   blankRow = $( ".prototype-row" ).clone();
+   event.preventDefault();
 
-$( function() {
-   $( "#add-row" ).click( function() {
-      var blankRow = $( ".prototype-row" ).clone();
-      event.preventDefault();
+   blankRow.toggleClass( "prototype-row test-row" );
+   blankRow.toggle();
+   blankRow.clone().insertBefore( "#stats-footer" );
+   x = $( "#form1 .test-row" ).last().prev().find( "select :selected" ).val();
+   if( typeof x != "undefined" ) $( "#form1 .test-row" ).last().find( "select" ).val( x ).prop( "selected", true );
+};
 
-      blankRow.toggleClass( "prototype-row test-row" );
-      blankRow.toggle();
-      blankRow.clone().insertBefore( "#stats-footer" );
-      x = $( "#form1 .test-row" ).last().prev().find( "select :selected" ).val();
-      if( typeof x != "undefined" ) $( "#form1 .test-row" ).last().find( "select" ).val( x ).prop( "selected", true );
-   });
-
-});
-
+// form serializer for calculator submission
 function serializeForm( form ) {
    json = [];
    $( form ).find( "div.test-row" ).each( function() {
